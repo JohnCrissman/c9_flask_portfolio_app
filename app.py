df03871cc506512a37613563173cc939cc68d26c
JohnCrissman <40966807+JohnCrissman@users.noreply.github.com>
2018-09-22 18:39:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + "likes to go to the beach."
+	new_name = name + " likes to go to the beach."
 	return render_template('index.html', name=new_name)
 
 
